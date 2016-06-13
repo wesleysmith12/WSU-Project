573672cea15604f7d893e565c8d79c286fa9e915
Wesley Smith <wesultimatef@gmail.com>
2016-06-13 15:04:33 -0600

    First draft of user interface

---
+++ app/src/main/java/com/example/wesle/wsuuioption1/Fragment1.java
@@ -0,0 +1,18 @@
+package com.example.wesle.wsuuioption1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/wesle/wsuuioption1/TravelBagFragment.java
@@ -0,0 +1,18 @@
+package com.example.wesle.wsuuioption1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TravelBagFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.travel_bag_fragment, container, false);
+        return view;
+    }
+}
